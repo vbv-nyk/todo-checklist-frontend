b8257a189a7b1acd813937d8d0d5842d2f610a68
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-01 21:42:43 +0530

    Almost made a smiley face

--- components/Chart.js
+++ components/Chart.js
@@ -3,6 +3,33 @@ import * as d3 from "d3";
 export default function Chart() {
     return (
         <>
+            <svg width="300" height="300">
+                <circle
+                    cx={300 / 2}
+                    cy={300 / 2}
+                    r="100"
+                    fill="yellow"
+                    stroke="black"
+                    strokeWidth={10}
+                >
+                </circle>
+                <circle
+                    cx={250 / 2}
+                    cy={250 / 2}
+                    r="15"
+                    strokeWidth={10}
+                >
+
+                </circle>
+                <circle
+                    cx={350 / 2}
+                    cy={250 / 2}
+                    r="15"
+                    strokeWidth={10}
+                >
+
+                </circle>
+            </svg>
         </>
     )
 }
