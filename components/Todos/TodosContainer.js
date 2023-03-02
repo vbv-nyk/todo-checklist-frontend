87ada893a9848c29d956266941f6ca70b8d68dc0
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-02 20:07:47 +0530

    Created todos header component

--- components/Todos/TodosContainer.js
+++ components/Todos/TodosContainer.js
@@ -1,14 +1,6 @@
 import Todo from "./Todo";
+import { TodosHeader } from "./TodosHeader";
 
-function TodosHeader() {
-    return (<div className="flex items-center justify-between p-4">
-        <div className="text-xl font-bold text-white">Your Todos</div>
-        <div className="flex gap-2 font-bold">
-            <button className="px-3 py-2 rounded-sm bg-slate-600">Add</button>
-            <button className="px-3 py-2 rounded-sm bg-slate-600">Sort</button>
-        </div>
-    </div>)
-}
 export default function TodosContainer() {
     return (<div className="flex flex-col gap-1 p-6 bg-slate-700">
         <TodosHeader />
