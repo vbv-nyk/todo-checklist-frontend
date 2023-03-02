052919aa86401961155ee20c0d379ada61f858ab
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-02 20:05:13 +0530

    Styled todo box

--- components/Todos/Todo.js
+++ components/Todos/Todo.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Todo({ title, note, link, iconURL, start, end }) {
-    return (<div className="flex flex-col items-start justify-start gap-2 p-4 shadow-md bg-slate-700 shadow-black ">
+    return (<div className="flex flex-col items-start justify-start gap-2 p-4 rounded-lg shadow-lg shadow-slate-800 bg-slate-600 ">
         <div className="flex flex-row items-center gap-2 shrink-0">
             <Image src={iconURL} alt={"Image Url"} height={5} width={20} />
             <a href={link} className={"font-semibold text-lg"}>{title}</a>
--- components/Todos/TodosContainer.js
+++ components/Todos/TodosContainer.js
@@ -1,15 +1,18 @@
 import Todo from "./Todo";
 
-export default function TodosContainer() {
-    return (<div className="flex flex-col gap-1 p-6 bg-slate-800">
-        <div className="flex items-center justify-between p-4">
-            <div className="text-xl font-bold text-white">Your Todos</div>
-            <div className="flex gap-2 font-bold">
-                <button className="px-3 py-2 bg-slate-700">Add</button>
-                <button className="px-3 py-2 bg-slate-700">Sort</button>
-            </div>
+function TodosHeader() {
+    return (<div className="flex items-center justify-between p-4">
+        <div className="text-xl font-bold text-white">Your Todos</div>
+        <div className="flex gap-2 font-bold">
+            <button className="px-3 py-2 rounded-sm bg-slate-600">Add</button>
+            <button className="px-3 py-2 rounded-sm bg-slate-600">Sort</button>
         </div>
-        <div className="flex flex-col gap-3">
+    </div>)
+}
+export default function TodosContainer() {
+    return (<div className="flex flex-col gap-1 p-6 bg-slate-700">
+        <TodosHeader />
+        <div className="flex flex-col gap-2">
             <Todo title="Todo 1" note="Note 1" link="http://www.google.com" iconURL="https://img.icons8.com/office/30/null/google-logo.png" />
             <Todo title="Todo 1" note="Note 1" link="http://www.google.com" iconURL="https://img.icons8.com/office/30/null/google-logo.png" />
             <Todo title="Todo 1" note="Note 1" link="http://www.google.com" iconURL="https://img.icons8.com/office/30/null/google-logo.png" />
