6d0bc9250e3c7d47e83db4ac2357322cd88b42d7
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-05 00:22:31 +0530

    Add calendar with functioning colors for intensity of usage

--- components/Chart.js
+++ components/Chart.js
@@ -1,32 +1,47 @@
-import React, { useRef, useEffect } from 'react';
-import * as d3 from 'd3';
+import React from 'react';
+import CalendarHeatmap from 'react-calendar-heatmap';
 
-function TodoHeatmap({ data }) {
-    const date = new Date();
-    const month = date.getMonth() + 1;
+const TodoCalendar = ({ todos }) => {
+    // Create a new Map to store the todo counts for each day
+    const todoCounts = new Map();
 
-    function getNoOfDays() {
-        //December or January
-        if (month === 12 || month === 1) {
-            return 31;
-        }
+    // Loop through each todo and increment the count for the day it was created
+    todos.forEach(todo => {
+        const createdAt = new Date(todo.date);
+        const year = createdAt.getFullYear();
+        const month = createdAt.getMonth();
+        const day = createdAt.getDate();
+        const dateKey = `${year}-${month}-${day}`;
 
-        //Febuary
-        if (month === 2) { return 28; }
-
-        //Other months, 31 days if even else 30 days
-        if (month % 2) {
-            return 31;
+        if (todoCounts.has(dateKey)) {
+            todoCounts.set(dateKey, todoCounts.get(dateKey) + 1);
         } else {
-            return 30;
+            todoCounts.set(dateKey, 1);
         }
-    }
+    });
 
-    console.log(getNoOfDays());
+    // Convert the todo counts Map to an array of objects that can be passed to the CalendarHeatmap
+    const data = Array.from(todoCounts, ([date, count]) => ({ date, count }));
+
     return (
-        <svg height={200} className="w-full px-6">
-        </svg >
-    )
-}
+        <CalendarHeatmap
+            startDate={new Date('2023-01-01')} // Start date of the calendar
+            endDate={new Date('2023-06-01')} // End date of the calendar (defaults to current date)
+            values={data} // Array of objects with `date` and `count` properties
+            classForValue={value => {
+                if (!value) {
+                    return 'color-empty';
+                }
+                return `color-scale-${value.count}`;
+            }} // CSS class name to apply based on the count value
+            tooltipDataAttrs={value => {
+                return {
+                    'data-tip': `${value.date}: ${value.count} todos created`
+                };
+            }} // Tooltip data attributes
+            showWeekdayLabels={true} // Whether to show weekday labels at the top of the calendar
+        />
+    );
+};
 
-export default TodoHeatmap;
+export default TodoCalendar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <div >
       <Navbar />
-      <TodoHeatmap />
       <TodosContainer />
     </div>
   )
