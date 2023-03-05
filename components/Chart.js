7942743c4a6a812bdd7971d543174cc81f47438e
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-05 10:55:38 +0530

    Showing done todos

--- components/Chart.js
+++ components/Chart.js
@@ -1,49 +1,8 @@
 import React from 'react';
-import CalendarHeatmap from 'react-calendar-heatmap';
 
 const TodoCalendar = ({ todos }) => {
-
-    const today = new Date();
-    // Create a new Map to store the todo counts for each day
-    const todoCounts = new Map();
-
-    // Loop through each todo and increment the count for the day it was created
-    todos.forEach(todo => {
-        const createdAt = new Date(todo.date);
-        const year = createdAt.getFullYear();
-        const month = createdAt.getMonth();
-        const day = createdAt.getDate();
-        const dateKey = `${year}-${month}-${day}`;
-
-        if (todoCounts.has(dateKey)) {
-            // Increment the count for this date if it has already been added to the map
-            todoCounts.set(dateKey, todoCounts.get(dateKey) + 1);
-        } else {
-            // Add a new entry to the map with a count of 1 if this date hasn't been added yet
-            todoCounts.set(dateKey, 1);
-        }
-
-    });
-
-    // Convert the todo counts Map to an array of objects that can be passed to the CalendarHeatmap
-    const data = Array.from(todoCounts, ([date, count]) => ({ date, count }));
-
     return (
-        <CalendarHeatmap
-            startDate={new Date('2023-01-01')} // Start date of the calendar
-            endDate={new Date('2023-04-01')} // End date of the calendar (defaults to current date)
-            values={data} // Array of objects with `date` and `count` properties
-            classForValue={value => {
-                if (!value) {
-                    return 'color-empty';
-                }
-                return `color-scale-${value.count}`;
-            }} // CSS class name to apply based on the count values
-            showWeekdayLabels={true} // Whether to show weekday labels at the top of the calendar
-            titleForValue={value => {
-                return value ? `${value.date}: ${value.count} todos created` : null;
-            }}
-        />
+        <></>
     );
 };
 
--- components/Todos/Todo.js
+++ components/Todos/Todo.js
@@ -9,6 +9,9 @@ export default function Todo({ title, note, link, iconURL, id, done }) {
     const titleRef = useRef(null);
     const noteRef = useRef(null);
 
+    let titleMarkup = title;
+    let noteMarkup = note;
+
     async function updateTodo() {
         const title = titleRef.current.value;
         const note = noteRef.current.value;
@@ -51,16 +54,19 @@ export default function Todo({ title, note, link, iconURL, id, done }) {
         )
     }
 
+
     return (<div className="flex flex-col items-start justify-start gap-2 p-4 rounded-lg shadow-lg shadow-slate-800 bg-slate-600 ">
         <div className="flex flex-row items-center gap-2 shrink-0">
             <div className="w-auto h-auto">
                 <Image src={iconURL} alt={"Image Url"} height={5} width={20} />
             </div>
-            {link !== "" ? <a href={link} target={"_blank"} className={"font-semibold text-lg"}>{title}</a> : <div className="text-lg font-semibold">{title}</div>}
+            {link !== "" ? <a href={link} target={"_blank"} className={"font-semibold text-lg"}>{titleMarkup}</a> : <div className="text-lg font-semibold">{titleMarkup}</div>}
             {!done && <div>
                 <TodoOptions id={id} setEditing={setEditing} />
+
             </div>}
+            {done && <div className="text-green-400">Done</div>}
         </div>
-        <div className="break-all">{note}</div>
+        <div className="break-all">{noteMarkup}</div>
     </div>)
 } 
